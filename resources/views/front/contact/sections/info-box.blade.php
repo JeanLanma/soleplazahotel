f35068dbb545b6010b92a7a9572c4167bdeea18b
JeanLanma <jeanmacario048@gmail.com>
2026-04-29 15:06:08 -0600

    UPDATE\ Cleaning Branding

--- resources/views/front/contact/sections/info-box.blade.php
+++ resources/views/front/contact/sections/info-box.blade.php
@@ -9,12 +9,12 @@
             <ul class="text-sm font-bold flex flex-col gap-10 text-center">
                 <li>
                     <p>
-                        Avenida Miguel Hidalgo y Costilla #656, C.P. 44100 Guadalajara, Jalisco, México
+                        Av. Adolfo López Mateos Sur 2400, Cd. de Sol, 45050, Guadalajara, Jalisco.
                     </p>
                 </li>
                 <li>
                     <p>
-                        +52 (33) 3658 1051
+                        +52 (33) 0000 0000
                     </p>
                 </li>
                 <li>
--- resources/views/front/contact/sections/map.blade.php
+++ resources/views/front/contact/sections/map.blade.php
@@ -4,7 +4,7 @@
     <div class="space-y lg:hidden"></div>
    
     <div class="h-96">
-        <iframe class="w-full h-full object-cover" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14931.235824195926!2d-103.3525861!3d20.67735!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428b1e4aea978ed%3A0x44d32412060cb052!2sHotel%20Jorge%20Alejandro!5e0!3m2!1sen!2smx!4v1731350583423!5m2!1sen!2smx" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="w-full h-full object-cover" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3733.5133691409824!2d-103.40736172475374!3d20.648683080907215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428adcd53bcba9f%3A0x9f891a65e785fdda!2sAv.%20Adolfo%20L%C3%B3pez%20Mateos%20Sur%202400%2C%20Cd%20del%20Sol%2C%2045050%20Guadalajara%2C%20Jal.!5e0!3m2!1ses-419!2smx!4v1777496594494!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
 </section>
